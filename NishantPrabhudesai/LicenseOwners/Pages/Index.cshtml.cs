455c732dd6d4356253d104929c142abd46cd8824
NishantUC <54602237+NishantUC@users.noreply.github.com>
2019-11-10 20:47:57 -0500

    Project Commit

---
+++ NishantPrabhudesai/LicenseOwners/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuickType;
+
+namespace LicenseOwners.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            using (var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://data.cityofchicago.org/resource/r5kz-chrr.json");
+                var businessLicenses = BusinessLicenses.FromJson(jsonString);
+                ViewData["BusinessLicenses"] = businessLicenses;
+            }
+             
+        }
+    }
+}
